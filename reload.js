e4ca209495909933d71ccd33f0d4a80cd9d93ee3
Akshat Sharma <Akshatsharmain11@gmail.com>
2024-07-04 09:54:53 +0530

    new bg

--- reload.js
+++ reload.js
@@ -9,3 +9,7 @@ document.getElementById("reload1").onclick = function() {
 document.getElementById("reload2").onclick = function() {
     location.reload();
 };
+
+document.getElementById("reload3").onclick = function() {
+    location.reload();
+};
